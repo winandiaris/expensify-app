63e7a2ede9cb64362200a8c9df1dae6951d48b02
aris winandi <winandiaris@gmail.com>
2019-10-05 14:44:02 +0700

    setting webpack untuk extract css - minicss

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,103 @@
 
 const path = require('path')
+// const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const devMode = process.env.NODE_ENV !== 'production'
 
 
-module.exports = {
+module.exports = (env) => {
+    const isProduction = env === 'production'
+    //const CSSExtract = new ExtractTextPlugin('styles.css')
 
-    entry : path.resolve(__dirname, './src/app.js') ,// path direktori file/script utama 
-    //entry : './public/src/app.js',
-    output : {
-        path : path.join(__dirname, 'public'), // path direktori output file webpack
-        
-        filename : "bundle.js", //nama file webpack
-        //publicPath: '/public/'
-    },
-
-    module :{
-        rules : [{
-            loader : 'babel-loader',
-            test : /\.js$/, //regex untuk mencari extention .js
-            exclude : /node_modules/
+    console.log('env', env)
+    return {
+        entry : path.resolve(__dirname, './src/app.js') ,// path direktori file/script utama 
+        //entry : './public/src/app.js',
+        output : {
+            path : path.join(__dirname, 'public'), // path direktori output file webpack
+            
+            filename : "bundle.js", //nama file webpack
+            //publicPath: '/public/'
         },
-        {
-            test : /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }
     
-    ]
-    },
+        module :{
+            rules : [{
+                loader : 'babel-loader',
+                test : /\.js$/, //regex untuk mencari extention .js
+                exclude : /node_modules/
+            },
+            {
+                test : /\.s?css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: process.env.NODE_ENV === 'development',
+                          },
 
-    devtool : 'cheap-module-eval-source-map',
-    devServer : {
+                        //   loader: 'css-loader',
+                        //   options: {
+                        //       sourceMap: true
+                        //   },
+
+                        //   loader: 'sass-loader',
+                        //       options: {
+                        //           sourceMap: true
+                        //       }
+                    },
+                    // {
+                    //           loader: 'css-loader',
+                    //           options: {
+                    //               sourceMap: true
+                    //           }
+                    //       ,
+                          
+                    //           loader: 'sass-loader',
+                    //           options: {
+                    //               sourceMap: true
+                    //           }  
+                    // }     
+                    'css-loader',
+                    'sass-loader',
+                    
+
+                 ]
+
+                // use: 
+                // //CSSExtract.extract
+                // // ExtractTextPlugin.extract
+                // ({
+                //     use: [
+                //         'style-loader',
+                //         'css-loader',
+                //         'sass-loader'
+                //     ]
+                // })
+            }
         
-        contentBase : './public',
-        historyApiFallback: true
+        ]
+        },
 
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: devMode ? '[name].css' : '[name].[hash].css',
+                chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+            })
+        ],
+
+        // plugins: [
+        //     //CSSExtract
+        //     new ExtractTextPlugin('styles.css')
+        // ],
+    
+        devtool : isProduction ? 'source-map' : 'cheap-module-eval-source-map', //diganti dibawah, katanya banyak bug
+        // devtool : isProduction ? 'source-map' : 'inline-source-map',
+        devServer : {
+            
+            contentBase : './public',
+            historyApiFallback: true
+    
+        }
     }
+    
 }
